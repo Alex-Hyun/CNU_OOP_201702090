7fa9a57944a3a591a09e5284ec451e2101c879e9
hsh1769 <hsh1769@o.cnu.ac.kr>
2020-09-23 00:37:56 +0900

    4주차 과제

--- CNU_OOP_WEEK4/homework01.c
+++ CNU_OOP_WEEK4/homework01.c
@@ -1,13 +1,13 @@
-//#include <stdio.h>
-//#include <Windows.h>
-//
-//int main() {
-//	char s[101] = ""; // 최대 100자의 문자열 입력
-//	printf("문자를 입력하세요(100자 이내).\n");
-//	scanf_s("%s", &s, sizeof(s));
-//
-//	printf("입력된 문자는 %s 입니다.\n", s);
-//	
-//	system("pause"); // keep terminal open
-//	return 0;
-//}
+#include <stdio.h>
+#include <Windows.h>
+
+int main() {
+	char s[101] = ""; // 최대 100자의 문자열 입력
+	printf("문자를 입력하세요(100자 이내).\n");
+	scanf_s("%s", &s, sizeof(s));
+
+	printf("입력된 문자는 %s 입니다.\n", s);
+	
+	system("pause"); // keep terminal open
+	return 0;
+}
